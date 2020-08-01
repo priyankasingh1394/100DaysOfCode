a3fd5d38d69a086309180ee16876620072bcaeb9
priyankasingh1394 <me.rani94@gmail.com>
2020-08-01 23:16:49 +0530

    Javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+var args = []; // Empty array, at first.
+for (var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i])
+} // Now 'args' is an array that holds your arguments.
+
+<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
+integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
